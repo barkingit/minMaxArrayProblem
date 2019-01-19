bc6c163553357f1cea12379584408bb4f7f8d028
barkintek <barkintekn@gmail.com>
2019-01-19 15:38:29 +0300

    Deleted lines

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -35,12 +35,6 @@ public class Solution {
     public static void main(String[] args) {
         int[] arr = new int[5];
 
-        arr[0]=769082435;
-        arr[1]=210437958;
-        arr[2]=673982045;
-        arr[3]=375809214;
-        arr[4]=380564127;
-
 //        for (int i = 0; i < 5; i++) {
 //            int arrItem = Integer.parseInt(arrItems[i]);
 //            arr[i] = arrItem;
